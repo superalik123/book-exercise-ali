462984af484c5f73028abc43a4423d6f46fb6520
superalik123 <44001727+superalik123@users.noreply.github.com>
2019-02-13 01:34:20 -0500

    Add files via upload

--- Book.java
+++ Book.java
@@ -15,10 +15,12 @@ class Book
     private int pages;
     //2.88
     private String refNumber;
-    private String borrowed;
     
+    private int borrowed;
+     
+    private boolean courseText;
    
-    public int getPages(int pages)
+    public int getPages()
         {
             return pages;
         }
@@ -27,12 +29,13 @@ class Book
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle, int bookPages)
+    public Book(String bookAuthor, String bookTitle, int bookPages, boolean courseText)
     {
         author    =  bookAuthor;
         title     =  bookTitle;
         pages     =  bookPages;
         refNumber =  "";
+        
     }
     //2.83
     public String getAuthor()
@@ -55,6 +58,7 @@ class Book
         System.out.println("Title:"+title);
         System.out.println("Author:"+author);
         System.out.println("Pages:"+pages);
+        System.out.println("Number of times this book has been borrowed: "+borrowed);
         if(refNumber.length()>0)
         System.out.println("Ref: "+refNumber);
         else
@@ -73,5 +77,17 @@ class Book
     else
     {System.out.println("Ref must be atleast 3 characters.");
     }
+    }
+    //2.91 
+    public int getBorrowed(){
+        return borrowed;
+    }
+    public void borrow(){
+        
+        borrowed = borrowed + 1;
+    }
+    //2.92
+    public boolean isCourseText(){
+        return courseText;
     }
 }
